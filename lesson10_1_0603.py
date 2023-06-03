3cddb19ad5788b08d36fddffb28860ecd35744ff
Catherine <mina123779@gmail.com>
2023-06-03 13:53:20 +0800

    0603

--- lesson10_1_0603.py
+++ lesson10_1_0603.py
@@ -1,6 +1,6 @@
-import streamlit as st
+import streamlit as st 
 
-if st.button('Say hello',key="myButton"): #mybutton=false
+if st.button('Say hello',key="myButton"): #mybutton=false #程式都是從上而下讀一次的
     st.write('Why hello there')
 else:
     st.write('Goodbye')
