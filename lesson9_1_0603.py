e79fc701d2c139f5a0817a02bee604c817b1d850
Catherine <mina123779@gmail.com>
2023-06-03 10:37:24 +0800

    0603

--- lesson9_1_0603.py
+++ lesson9_1_0603.py
@@ -1,4 +1,18 @@
 import streamlit as st
 
+
+
 st.write("#### Hello world")
 st.write("Hello ***Catherine***")
+st.markdown(" **Hi world!**")
+st.divider()##分隔線
+st.title("這是app的title")
+st.divider()##分隔線
+st.header("這是header")
+st.divider()##分隔線
+st.subheader("這是subheader")
+st.divider()##分隔線
+st.caption("這是caption")
+st.divider()##分隔線
+st.code("a=123")
+
