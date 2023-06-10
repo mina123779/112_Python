38dd90a9e9f1ec16952db73e41ff31931cd3c60a
Catherine <132725460+mina123779@users.noreply.github.com>
2023-06-10 06:54:17 +0000

    0610

--- lesson12_1_0610.py
+++ lesson12_1_0610.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import pandas as pd
+
+codeFrame = pd.read_csv('codeSearch.csv',usecols=['code','name'])
+codeSeries = codeFrame['code'].astype(str) + codeFrame['name']
 
 with st.sidebar:
-    st.write("請選擇股票號碼:")
+    st.multiselect("請選擇股票:",codeSeries)
